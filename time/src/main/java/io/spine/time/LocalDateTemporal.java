6228037d5ca6a46129d87c132441ad0781aede53
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2019-03-29 17:02:31 +0200

    Make `LocalDate` temporal.

---
+++ time/src/main/java/io/spine/time/LocalDateTemporal.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.time;
+
+import com.google.protobuf.Timestamp;
+import io.spine.time.temporal.TemporalMessage;
+
+import java.time.Instant;
+
+import static io.spine.protobuf.Timestamps2.fromInstant;
+import static java.time.ZoneOffset.UTC;
+
+interface LocalDateTemporal extends TemporalMessage<LocalDate>, LocalDateOrBuilder {
+
+    @Override
+    default Timestamp toTimestamp() {
+        Instant instant = java.time.LocalDate
+                .of(getYear(), getMonthValue(), getDay())
+                .atStartOfDay()
+                .toInstant(UTC);
+        Timestamp result = fromInstant(instant);
+        return result;
+    }
+}
