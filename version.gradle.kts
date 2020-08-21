e7754fd92ce1e5545c9e8b2b591f7a39707fa392
serhii.lekariev <serhii.lekariev@teamdev.com>
2020-08-21 09:58:16 +0300

    Explain the version naming.

--- version.gradle.kts
+++ version.gradle.kts
@@ -18,10 +18,30 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-val versionToPublish = "1.5.22"
+/**
+ * The versions of the libraries used.
+ *
+ * This file is used in both module `build.gradle.kts` scripts and in the integration tests,
+ * as we want to manage the versions in a single source.
+ *
+ * This version file adheres to the contract of the
+ * [publishing application](https://github.com/SpineEventEngine/publishing).
+ *
+ * When changing the version declarations or adding new ones, make sure to change
+ * the publishing application accordingly.
+ */
+
+/**
+ * Version of this library.
+ */
+val time = "1.5.22"
+
+/**
+ * Versions of the Spine libraries that `time` depends on.
+ */
 val base = "1.5.22"
 
 project.extra.apply {
-    this["versionToPublish"] = versionToPublish
+    this["versionToPublish"] = time
     this["spineBaseVersion"] = base
 }
