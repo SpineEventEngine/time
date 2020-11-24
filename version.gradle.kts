4f97d4976dd5bfa8f0e1ffe9de574d9c84f2618b
Alex Tymchenko <alex.tymchenko@teamdev.com>
2020-11-24 17:26:38 +0200

    Back-merge from `master`. Create a `pull` script in the repo root to avoid any branch-switching issues in future. Update to the latest `base@2.x` version.

--- version.gradle.kts
+++ version.gradle.kts
@@ -34,12 +34,12 @@
 /**
  * Version of this library.
  */
-val time = "2.0.0-jdk8.SNAPSHOT.6"
+val time = "2.0.0-jdk8.SNAPSHOT.8"
 
 /**
  * Versions of the Spine libraries that `time` depends on.
  */
-val base = "2.0.0-jdk8.SNAPSHOT.6"
+val base = "2.0.0-jdk8.SNAPSHOT.8"
 
 project.extra.apply {
     this["versionToPublish"] = time
