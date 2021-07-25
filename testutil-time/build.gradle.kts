f488c8c981c0dafc285a4f5729fd1277585d642e
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2021-07-25 23:26:36 +0300

    Set dup. strategy

--- testutil-time/build.gradle.kts
+++ testutil-time/build.gradle.kts
@@ -30,3 +30,9 @@ val spineBaseVersion: String by extra
 dependencies {
     api(project(":time"))
 }
+
+//TODO:2021-07-22:alexander.yevsyukov: Turn to WARN and investigate duplicates.
+// see https://github.com/SpineEventEngine/base/issues/657
+val dupStrategy = DuplicatesStrategy.INCLUDE
+tasks.processResources.get().duplicatesStrategy = dupStrategy
+tasks.sourceJar.get().duplicatesStrategy = dupStrategy
