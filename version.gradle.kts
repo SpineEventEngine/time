b03eb3e30e1e9f061194b920acbe0aa587f2f6fb
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2021-07-25 22:40:16 +0300

    Bump version

--- version.gradle.kts
+++ version.gradle.kts
@@ -40,12 +40,12 @@
 /**
  * Version of this library.
  */
-val time = "2.0.0-SNAPSHOT.34"
+val time = "2.0.0-SNAPSHOT.39"
 
 /**
  * Versions of the Spine libraries that `time` depends on.
  */
-val base = "2.0.0-SNAPSHOT.34"
+val base = "2.0.0-SNAPSHOT.39"
 
 project.extra.apply {
     this["versionToPublish"] = time
