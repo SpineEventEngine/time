95d73119a03ac05a6606b15819c34ad733f037be
yevhenii.nadtochii <yevhenii.nadtochii@teamdev.com>
2022-05-27 14:57:12 +0300

    Implement `BundleEntry`

---
+++ version-catalog/src/test/kotlin/io/spine/internal/catalog/entry/bundle/given/Standalone.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2022, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.internal.catalog.entry.bundle.given
+
+import io.spine.internal.catalog.entry.BundleEntry
+import io.spine.internal.catalog.entry.LibraryEntry
+import io.spine.internal.catalog.entry.VersionEntry
+
+@Suppress("unused")
+internal object StandaloneDummy : VersionEntry() {
+
+    private const val group = "ord.dummy"
+    override val version = "dl-1.2.3"
+
+    object Lib1 : LibraryEntry() {
+        override val module = "$group:dummy-lib1"
+    }
+    object Lib2 : LibraryEntry() {
+        override val module = "$group:dummy-lib2"
+    }
+    object Lib3 : LibraryEntry() {
+        override val module = "$group:dummy-lib3"
+    }
+
+    object Libs : BundleEntry() {
+        override val bundle = setOf(Lib1, Lib2, Lib3)
+    }
+}
