7d3d36a51f8c30859f20f644859ea16c3e2c94fd
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-10-21 16:18:36 +0300

    Remove manual adding of `kotlin` built-in

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,15 +64,17 @@ buildscript {
     dependencies {
         classpath(spine.mcJavaPlugin)
     }
+
+    val jackson = io.spine.internal.dependency.Jackson
     configurations {
         all {
             resolutionStrategy {
                 force(
                     spine.base,
-                    io.spine.internal.dependency.Jackson.annotations,
-                    io.spine.internal.dependency.Jackson.bom,
-                    io.spine.internal.dependency.Jackson.databind,
-                    io.spine.internal.dependency.Jackson.moduleKotlin
+                    jackson.annotations,
+                    jackson.bom,
+                    jackson.databind,
+                    jackson.moduleKotlin
                 )
             }
         }
@@ -216,7 +218,6 @@ subprojects {
     val generatedDir:String by extra("$projectDir/generated")
 
     protobuf {
-        generatedFilesBaseDir = generatedDir
         protoc {
             // Temporarily use this version, since 3.21.x is known to provide
             // a broken `protoc-gen-js` artifact.
@@ -228,7 +229,6 @@ subprojects {
         generateProtoTasks {
             all().forEach { task ->
                 task.builtins {
-                    maybeCreate("kotlin")
                     id("js") {
                         option("library=spine-time-${project.version}")
                     }
