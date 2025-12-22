ed2b18265ab2e0d63f098966abc4393dcb1b7cdd
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-12-22 18:22:24 +0000

    Remove extra empty line

--- buildSrc/src/main/kotlin/io/spine/dependency/local/CoreJvmCompiler.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/CoreJvmCompiler.kt
@@ -29,7 +29,6 @@ package io.spine.dependency.local
 import io.spine.dependency.local.CoreJvmCompiler.dogfoodingVersion
 import io.spine.dependency.local.CoreJvmCompiler.version
 
-
 /**
  * Dependencies on the CoreJvm Compiler artifacts.
  *
