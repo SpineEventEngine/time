892bf1e139b3c45e83887d9a1c97e8304e8f1fdc
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-03-10 18:28:16 +0000

    Add `LocalDateValidator`

---
+++ time/src/main/kotlin/io/spine/time/validation/LocalDateValidator.kt
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2026, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.time.validation
+
+import com.google.auto.service.AutoService
+import io.spine.base.FieldPath
+import io.spine.time.LocalDate
+import io.spine.time.Month
+import io.spine.validation.DetectedViolation
+import io.spine.validation.FieldViolation
+import io.spine.validation.MessageValidator
+import io.spine.validation.RuntimeErrorPlaceholder.FIELD_PATH
+import io.spine.validation.RuntimeErrorPlaceholder.RANGE_VALUE
+import io.spine.validation.templateString
+import java.time.YearMonth
+
+/**
+ * Validates [LocalDate] messages.
+ *
+ * Ensures that the day of a month is within the range allowed for the given month.
+ * This takes into account the number of days in February in leap years.
+ */
+@AutoService(MessageValidator::class)
+public class LocalDateValidator : MessageValidator<LocalDate> {
+
+    override fun validate(message: LocalDate): List<DetectedViolation> {
+        val year = message.year
+        val month = message.month
+        val day = message.day
+
+        if (month == Month.MONTH_UNDEFINED || month == Month.UNRECOGNIZED) {
+            return emptyList()
+        }
+
+        val daysInMonth = YearMonth.of(year, month.number).lengthOfMonth()
+        if (day > daysInMonth) {
+            return listOf(invalidDay(day, daysInMonth))
+        }
+        return emptyList()
+    }
+}
+
+/**
+ * Creates a violation for an invalid day of the month.
+ *
+ * @param day the invalid day value.
+ * @param maxDays the maximum allowed days for the month.
+ */
+private fun invalidDay(day: Int, maxDays: Int): FieldViolation = FieldViolation(
+    message = templateString {
+        withPlaceholders = "The \${$FIELD_PATH} value is out of range" +
+                " (\${$RANGE_VALUE}): $day."
+        placeholderValue.put(FIELD_PATH.value, "day")
+        placeholderValue.put(RANGE_VALUE.value, "1..$maxDays")
+    },
+    fieldPath = FieldPath.newBuilder()
+        .addFieldName("day")
+        .build(),
+    fieldValue = day
+)
---
+++ time/src/test/kotlin/io/spine/time/validation/LocalDateValidatorSpec.kt
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2026, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.time.validation
+
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import io.spine.time.LocalDate
+import io.spine.time.Month
+import io.spine.validation.FieldViolation
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+@DisplayName("`LocalDateValidator` should")
+internal class LocalDateValidatorSpec {
+
+    private val validator = LocalDateValidator()
+
+    @Test
+    @DisplayName("allow valid dates")
+    fun allowValid() {
+        val date = LocalDate.newBuilder()
+            .setYear(2024)
+            .setMonth(Month.JANUARY)
+            .setDay(31)
+            .buildPartial()
+        validator.validate(date).shouldBeEmpty()
+    }
+
+    @ParameterizedTest
+    @DisplayName("detect invalid day for a month")
+    @CsvSource(
+        "2024, APRIL, 31, 30",
+        "2024, JUNE, 31, 30",
+        "2024, SEPTEMBER, 31, 30",
+        "2024, NOVEMBER, 31, 30",
+        "2023, FEBRUARY, 29, 28",
+        "2024, FEBRUARY, 30, 29"
+    )
+    fun detectInvalidDay(year: Int, month: Month, day: Int, maxDays: Int) {
+        val date = LocalDate.newBuilder()
+            .setYear(year)
+            .setMonth(month)
+            .setDay(day)
+            .buildPartial()
+        val violations = validator.validate(date)
+        violations shouldHaveSize 1
+        val violation = violations[0] as FieldViolation
+        violation.fieldPath!!.fieldNameList[0] shouldBe "day"
+        violation.fieldValue shouldBe day
+        violation.message.withPlaceholders shouldBe
+                "The \${field.path} value is out of range (\${range.value}): $day."
+        violation.message.placeholderValueMap["range.value"] shouldBe "1..$maxDays"
+    }
+
+    @Nested
+    @DisplayName("handle leap years and")
+    inner class LeapYear {
+
+        @Test
+        @DisplayName("allow Feb 29 in a leap year")
+        fun allowFeb29() {
+            val date = LocalDate.newBuilder()
+                .setYear(2024)
+                .setMonth(Month.FEBRUARY)
+                .setDay(29)
+                .buildPartial()
+            validator.validate(date).shouldBeEmpty()
+        }
+
+        @Test
+        @DisplayName("detect invalid Feb 29 in a non-leap year")
+        fun detectInvalidFeb29() {
+            val date = LocalDate.newBuilder()
+                .setYear(2023)
+                .setMonth(Month.FEBRUARY)
+                .setDay(29)
+                .buildPartial()
+            val violations = validator.validate(date)
+            violations shouldHaveSize 1
+            val fieldViolation = violations[0] as FieldViolation
+            fieldViolation.message.placeholderValueMap["range.value"] shouldBe "1..28"
+        }
+    }
+
+    @Test
+    @DisplayName("ignore undefined month")
+    fun ignoreUndefinedMonth() {
+        val date = LocalDate.newBuilder()
+            .setYear(2024)
+            .setMonth(Month.MONTH_UNDEFINED)
+            .setDay(31)
+            .buildPartial()
+        validator.validate(date).shouldBeEmpty()
+    }
+}
