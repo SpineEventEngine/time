621f0e3cc0d51ba3ca1c14272fd808b431c9f4cd
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2026-06-30 18:18:19 +0100

    Optimize imports

--- time/src/main/java/io/spine/time/ZoneOffsets.java
+++ time/src/main/java/io/spine/time/ZoneOffsets.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2023, TeamDev. All rights reserved.
+ * Copyright 2026, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -30,8 +30,8 @@ import com.google.protobuf.Duration;
 import com.google.protobuf.util.Durations;
 import io.spine.time.string.TimeStringifiers;
 import io.spine.util.SerializableConverter;
-
 import org.jspecify.annotations.Nullable;
+
 import java.io.Serial;
 import java.util.Locale;
 import java.util.TimeZone;
